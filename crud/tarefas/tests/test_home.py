160a3360d76bf44f4c35c784edcdf3407936c40b
diego <ruan.diego.anjos@outlook.com>
2024-06-09 18:34:15 -0300

    Conseguimos acessar o DB na página HTML

--- crud/tarefas/tests/test_home.py
+++ crud/tarefas/tests/test_home.py
@@ -7,20 +7,29 @@ from crud.tarefas.models import Tarefa
 
 @pytest.fixture
 def response_home(client: Client, db):
-    # Ao inves do client.get, use o client.post porque o método utilizado para criar dados e envia-los
+    return client.get(reverse('tarefas:home'), db)
+
+
+def test_title(response_home):
+    assert_cointains(response_home, '<title>Página Inicial</title>')
+
+
+def test_form_present(response_home):
+    assert_cointains(response_home, '<form')
+
+
+@pytest.fixture
+def response_redirect_home(client: Client, db):
+    # Ao invés do client.get, use o client.post porque o método utilizado para criar dados e envia-los
     return client.post(reverse('tarefas:home'), data={'nome': 'Tarefa'})
 
 
-def test_tarefa_existe_in_db(response_home):
+def test_tarefa_existe_in_db(response_redirect_home):
     assert Tarefa.objects.exists()
 
 
-def test_redirecionar_apos_salvar_no_db(response_home):
-    assert response_home.status_code == 302
-
-
-# def test_title(response_home):
-#     assert_cointains(response_home, 'Salvar</button>')
+def test_redirecionar_apos_salvar_no_db(response_redirect_home):
+    assert response_redirect_home.status_code == 302
 
 
 @pytest.fixture
@@ -34,3 +43,24 @@ def test_tarefa_nao_existe(dado_invalido):
 
 def test_pagina_com_dados_invalidos(dado_invalido):
     assert dado_invalido.status_code == 400
+
+
+@pytest.fixture
+def lista_de_tarefas_pendentes(db):
+    tarefas = [
+        Tarefa(nome='Primeira tarefa', feita=False),
+        Tarefa(nome='Segunda tarefa', feita=False),
+    ]
+    # Para criar um banco de dados
+    Tarefa.objects.bulk_create(tarefas)
+    return tarefas
+
+
+@pytest.fixture
+def response_com_lista_de_tarefas(client: Client, lista_de_tarefas_pendentes):
+    return client.get(reverse('tarefas:home'))
+
+
+def test_lista_de_tarefas_presentes(response_com_lista_de_tarefas, lista_de_tarefas_pendentes):
+    for tarefa in lista_de_tarefas_pendentes:
+        assert_cointains(response_com_lista_de_tarefas, tarefa.nome)
