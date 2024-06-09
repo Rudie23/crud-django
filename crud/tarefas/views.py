7aca6c75314b07fd13e3a3996eadc4eeccdb14a3
diego <ruan.diego.anjos@outlook.com>
2024-06-09 18:34:15 -0300

    Conseguimos acessar o DB na página HTML

--- crud/tarefas/views.py
+++ crud/tarefas/views.py
@@ -3,16 +3,21 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from crud.tarefas.forms import TarefaForm
+from crud.tarefas.models import Tarefa
 
 
 # Create your views here.
 def home(request):
+
     if request.method == 'POST':
         form = TarefaForm(request.POST)
         if form.is_valid():
+            # Dados válidos
             form.save()
             return HttpResponseRedirect(reverse('tarefas:home'))
         else:
-            return render(request, 'tarefas/home.html', {'form': form}, status=400)
-
-    return render(request, 'tarefas/home.html')
+            tarefas_pendentes = Tarefa.objects.filter(feita=False)
+            # Dados inválidos, e mostrar os erros no HTML page {{ form.name.erros }}
+            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+    tarefas_pendentes = Tarefa.objects.filter(feita=False)
+    return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
