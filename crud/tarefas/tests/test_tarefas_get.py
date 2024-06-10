23bdbd857ffe8a71ea973049721aa8a89a482ce8
diego <ruan.diego.anjos@outlook.com>
2024-06-09 22:20:09 -0300

    Deletar tarefa do BD pela página HTML

--- crud/tarefas/tests/test_tarefas_get.py
+++ crud/tarefas/tests/test_tarefas_get.py
@@ -19,33 +19,6 @@ def test_form_present(response_home):
 
 
 @pytest.fixture
-def response_redirect_home(client: Client, db):
-    # Ao invés do client.get, use o client.post porque o método utilizado para criar dados e envia-los
-    return client.post(reverse('tarefas:home'), data={'nome': 'Tarefa'})
-
-
-def test_tarefa_existe_in_db(response_redirect_home):
-    assert Tarefa.objects.exists()
-
-
-def test_redirecionar_apos_salvar_no_db(response_redirect_home):
-    assert response_redirect_home.status_code == 302
-
-
-@pytest.fixture
-def dado_invalido(client: Client, db):
-    return client.post(reverse('tarefas:home'), data={'name': ''})
-
-
-def test_tarefa_nao_existe(dado_invalido):
-    assert not Tarefa.objects.exists()
-
-
-def test_pagina_com_dados_invalidos(dado_invalido):
-    assert dado_invalido.status_code == 400
-
-
-@pytest.fixture
 def lista_de_tarefas_pendentes(db):
     tarefas = [
         Tarefa(nome='Primeira tarefa', feita=False),
