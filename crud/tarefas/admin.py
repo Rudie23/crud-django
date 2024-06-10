d369b54f39366df4b43711d42f9fa7075c21fed2
diego <ruan.diego.anjos@outlook.com>
2024-06-09 21:43:05 -0300

    Alteração dos dados do BD, pela página HTML, funcionando

--- crud/tarefas/admin.py
+++ crud/tarefas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from crud.tarefas.models import Tarefa
+
+
 # Register your models here.
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'feita')
+    list_filter = ('feita',)
+    ordering = ('-id',)
+    search_fields = ('name',)
--- crud/tarefas/models.py
+++ crud/tarefas/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Tarefa(models.Model):
     nome = models.CharField(max_length=256)
     feita = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
